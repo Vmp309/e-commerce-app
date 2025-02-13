86cc2a18b2f3169982ef9e0af73e2e206f42b9a5
Victoria Monteiro <vmp309@gmail.com>
2025-02-13 12:19:26 -0300

    Revert "Creation of models in progress"
    This reverts commit c598d1212d62ab887e4d25628e1552acd3f959bc.

--- backend/store/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .user import User
-from .product import Product
-from .order import Order
-from .category import Category
--- backend/store/models/user.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-import uuid
-
-class User(AbstractUser):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    birthday = models.DateField(null=True, blank=True) 
-    
